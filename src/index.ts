d879fe86bee4b38e85ebc726523edcb8bffbfcc4
Solanny <batistasolanny026@gmail.com>
2021-04-05 11:36:07 -1000

    Paginación y Limit

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,48 @@ import { retornaDocumentos } from './helpers/Mostrar-documentos';
 
 const usuariosRef= db.collection('usuarios');
 
+const btnNext = document.createElement('button');
+btnNext.innerText = 'New Page';
+document.body.append( btnNext )
 
-// select * from usuarios oder By nombre asc, salacio asc
-usuariosRef
-.orderBy('nombre')
-.orderBy('salario')
-.get().then( retornaDocumentos )
+let firstDocument: any= null;
+let lastDocument: any= null;
+btnNext.addEventListener('click', () =>{
+   
+   const query= usuariosRef
+                   .orderBy('nombre')
+                   .startAfter( lastDocument )
+
+
+   query.limit(2).get().then( snap => {
+    
+    lastDocument = snap.docs[ snap.docs.length - 1 ] || null;
+    retornaDocumentos(snap);
+   } )
+
+});
+
+
+//btn previous
+const btnPrev = document.createElement('button');
+btnPrev.innerText = 'Previous Page';
+document.body.append( btnPrev )
+
+btnPrev.addEventListener('click', () =>{
+   
+   const query= usuariosRef
+                   .orderBy('nombre')
+                   .endBefore( firstDocument )
+
+
+   query.limit(2).get().then( snap => {
+    
+    
+     
+        firstDocument = snap.docs[ 0 ] || null;
+        lastDocument = snap.docs[ snap.docs.length - 1 ] || null;
+    retornaDocumentos(snap);
+   } )
+
+});
+
