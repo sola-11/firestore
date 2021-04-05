b47e5f62fcce818dc7af83e9432a4a7ad87943b6
Solanny <batistasolanny026@gmail.com>
2021-04-05 11:11:31 -1000

    OrderBy

--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,11 @@
 import db from './firebase/config';
 import { retornaDocumentos } from './helpers/Mostrar-documentos';
 
-const usuario={
-    nombre: 'Josua',
-    activo: true,
-    fechaNaci: 0
-    
-}
 const usuariosRef= db.collection('usuarios');
 
- //db.collection('usuarios')
- // .add(usuario )
- // .then( docRef => {
- //     console.log( docRef )
- // })
-//  .catch( e => console.log( 'error' , e ))
 
-
-
-
-//usuariosRef
-// .doc('8VtQbf8xeakjjk3YRePL')
-// .update({
-//     activo: true
-// });
-
-
-//destructivo
-//usuariosRef
-// .doc('8VtQbf8xeakjjk3YRePL')
-// .set({
-//     activo: true,
-//     edad:44
-// });
-
-
-//delete from usuarios where id='xx'
-
-//usuariosRef
-//.doc('8VtQbf8xeakjjk3YRePL')
-//.delete()
-//.then( resp => console.log('Borrado') )
-//.catch( e => console.log('error' , e ));
-
-//select * from usuario;
-
-//usuariosRef
-//.onSnapshot( retornaDocumentos )
-
-//usuariosRef
-//.get().then(  retornaDocumentos );
-
-/*
-select * from usuario
- where activo= true
-*/
-
-//usuariosRef.where('activo' , '==' , true).get().then(  retornaDocumentos );
-
-
-/*
-select * from usuario
- where salario > 1800
-*/
-
-//usuariosRef.where('salario' , '>' , 1800)
-//.get().then(  retornaDocumentos );
-
-
-
-/*
-select * from usuario
- where salario > 1300 and salario < 2500
-*/
-
-//usuariosRef.where('salario' , '>=' , 1800)
-//.where('salario', '<=', 2500)
-//.get().then(  retornaDocumentos );
-
-
-/*
-select * from usuario
- where salario > 1800 
- And activo == true
- */
-
-usuariosRef.where('salario' , '>=' , 1800)
-.where('activo', '==', true)
-.get().then(  retornaDocumentos );
+// select * from usuarios oder By nombre asc, salacio asc
+usuariosRef
+.orderBy('nombre')
+.orderBy('salario')
+.get().then( retornaDocumentos )
